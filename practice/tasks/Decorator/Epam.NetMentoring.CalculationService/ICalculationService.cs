727c079fd2c6013390e9164fe0ac885926944e1b
Igor <Igor_infopol@ukr.net>
2015-01-05 15:31:10 +0200

    Calculation service updated

--- practice/tasks/Decorator/Epam.NetMentoring.CalculationService/ICalculationService.cs
+++ practice/tasks/Decorator/Epam.NetMentoring.CalculationService/ICalculationService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Epam.NetMentoring.CalculationService
+﻿namespace Epam.NetMentoring.CalculationService
 {
    public interface ICalculationService
     {
