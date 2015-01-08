428ad6831ebe51be75a3e69776e8b6ed3e2e697c
Igor <Igor_infopol@ukr.net>
2015-01-08 14:55:46 +0200

    Updated using costum type

--- practice/tasks/Decorator/Epam.NetMentoring.CalculationService/CachedCalculationService.cs
+++ practice/tasks/Decorator/Epam.NetMentoring.CalculationService/CachedCalculationService.cs
@@ -5,25 +5,49 @@ namespace Epam.NetMentoring.CalculationService
 {
     public class CachedCalculationService : ICalculationService
     {
+        private class Params
+        {
+            private readonly decimal _a;
+            private readonly decimal _b;
+
+            public Params(decimal a, decimal b)
+            {
+                _a = a;
+                _b = b;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null || GetType() != obj.GetType())
+                    return false;
+                var calcParams = (Params) obj;
+                return (calcParams._a == _a) && (calcParams._b == _b);
+            }
+
+            public override int GetHashCode()
+            {
+                return _a.GetHashCode() ^ _b.GetHashCode();
+            }
+        }
+
         private readonly ICalculationService _calculationService;
         private decimal _calcRes;
-        private readonly Dictionary<int, decimal> _cache = new Dictionary<int, decimal>();
+        private readonly Dictionary<Params, decimal> _cache = new Dictionary<Params, decimal>();
         public CachedCalculationService(ICalculationService calculationService)
         {
             _calculationService = calculationService;
         }
         public decimal Calculate(decimal a, decimal b)
         {
-            var hashCode = a.GetHashCode() ^ b.GetHashCode();
-
-            if (_cache.ContainsKey(hashCode))
+            var parms = new Params(a, b);
+            if (_cache.ContainsKey(parms))
             {
                 Console.WriteLine("Cached value found");
-                return _cache[hashCode];
+                return _cache[parms];
             }
 
             _calcRes = _calculationService.Calculate(a, b);
-            _cache.Add(hashCode, _calcRes);
+            _cache.Add(parms, _calcRes);
             return _calcRes;
         }
     }
