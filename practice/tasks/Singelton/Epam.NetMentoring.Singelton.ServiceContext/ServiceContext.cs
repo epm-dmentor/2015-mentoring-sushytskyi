7af1abf38ee315725e8a159751f58eda0bf730f7
Igor Tkachenko <legart.ua@gmail.com>
2015-02-12 12:07:27 +0200

    Singleton reviewed

--- practice/tasks/Singelton/Epam.NetMentoring.Singelton.ServiceContext/ServiceContext.cs
+++ practice/tasks/Singelton/Epam.NetMentoring.Singelton.ServiceContext/ServiceContext.cs
@@ -4,7 +4,10 @@ namespace Epam.NetMentoring.Singelton.ServiceContext
 {
     public class ServiceContext
     {
+        //IT: in you case volotile is not needed!
         private static volatile ServiceContext _context;
+
+        //IT: syncObj seems to be a better name
         private static readonly object Synch = new object();
         public Guid ConnectionId { get; private set; }
 
@@ -17,6 +20,8 @@ namespace Epam.NetMentoring.Singelton.ServiceContext
         {
             get
             {
+                //IT: each time when you request ServiceContext instance you will do lock, 
+                //but actualy we can avoid this lock for each execution
                 lock (Synch)
                 {
                     if (_context == null)
